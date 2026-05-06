a2af30a4e8b7fec56fccaafcaf42bce16fb8cdc8
Yaa-K <yaa.k.yeboah@outlook.com>
2026-05-06 16:30:00 +0000

    SPC-59: Added ACM certificate output

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -25,3 +25,8 @@ output "https_listener_arn" {
   description = "ARN of the HTTPS (port 443) listener. Useful if additional routing rules need to be added later."
   value       = aws_lb_listener.https.arn
 }
+
+output "acm_certificate_arn" {
+  description = "ARN of the ACM TLS certificate used by the ALB. Passed through from the module input for use by other modules that need to know which cert is in use."
+  value       = var.acm_certificate_arn
+}
