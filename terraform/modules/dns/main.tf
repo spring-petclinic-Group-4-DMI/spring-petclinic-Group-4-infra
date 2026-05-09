b4766b03452fa607d06bae946886e6d3a7190f24
Mustapha Nurudeen <mustaphanurudeen488@gmail.com>
2026-05-09 19:38:24 +0100

    ci: Bump Terraform version to 1.6.0 and remove backend and provider blocks from dns module

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -7,22 +7,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "spc-staging-ue1-tfstate"
-    key            = "dns/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "spc-staging-ue1-tfstate-lock"
-    encrypt        = true
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = var.default_tags
-  }
 }
 
 # ---------------------------------------------------------------------------
