e600dc7455e117155bdd34df7566bd5d65eceb95
Your Name <your.email@company.com>
2026-05-13 22:14:30 -0400

    changed OIDC set-up from resource to data 4

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,11 +1,11 @@
 output "github_actions_ci_role_arn" {
   description = "ARN of the GitHub Actions CI role for building and pushing to ECR"
-  value       = aws_iam_role.github_actions_ci.arn
+  value       = data.aws_iam_role.github_actions_ci.arn
 }
 
 output "terraform_role_arn" {
   description = "ARN of the Terraform role for provisioning infrastructure"
-  value       = aws_iam_role.terraform.arn
+  value       = data.aws_iam_role.terraform.arn
 }
 
 output "eks_node_role_arn" {
