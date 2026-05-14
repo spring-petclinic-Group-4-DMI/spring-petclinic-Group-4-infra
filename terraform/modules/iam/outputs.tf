b8fe737889a61b0c70eb76ee397b60eb21dad8e4
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2026-05-13 20:44:54 -0400

    SPC-004-removed -oidc-blocks-in-staging

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,8 +1,3 @@
-output "github_oidc_provider_arn" {
-  description = "ARN of the GitHub Actions OIDC provider"
-  value       = aws_iam_openid_connect_provider.github_oidc.arn
-}
-
 output "github_actions_ci_role_arn" {
   description = "ARN of the GitHub Actions CI role for building and pushing to ECR"
   value       = aws_iam_role.github_actions_ci.arn
